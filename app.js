2446d56b9dc7b230c14dbb3fa4d0fc276501874e
kebibragam <kebibragam@gmail.com>
2023-04-29 22:16:04 +0545

    added swagger

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -43,8 +48,10 @@ app.use(xss());
 
 //routes
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("<h1>Inventory API</h1><a href='/api-docs'>Documentation</a> ");
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/auth", authRouter);
 app.use(
