d253472ddf7e52f350c6caf0e97192bd2af4d29b
kebibragam <kebibragam@gmail.com>
2023-05-03 22:26:59 +0545

    transaction added

--- controllers/order.js
+++ controllers/order.js
@@ -2,6 +2,7 @@ const Order = require("../models/Order");
 const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const { mongoose } = require("mongoose");
 
 const getAllOrders = async (req, res) => {
   const orders = await Order.find({});
@@ -16,41 +17,64 @@ const getSingleOrder = async (req, res) => {
   res.status(StatusCodes.OK).json(order);
 };
 const createOrder = async (req, res) => {
-  // console.log(req.body);
-  const { items: cartItems } = req.body;
-  if (!cartItems || cartItems.length < 1) {
-    throw new CustomError.BadRequestError("No cart items provided");
-  }
-  let orderItems = [];
-  let total = 0;
-  for (const item of cartItems) {
-    const dbProduct = await Product.findOne({ _id: item.productId });
-    if (!dbProduct) {
-      throw new CustomError.NotFoundError(
-        `No product with id : ${item.productId}`
-      );
+  try {
+    // console.log(req.body);
+    const { items: cartItems } = req.body;
+    if (!cartItems || cartItems.length < 1) {
+      throw new CustomError.BadRequestError("No cart items provided");
     }
-    console.log(dbProduct);
-    const { name, price, image, _id, quantity } = dbProduct;
-    const singleOrderItem = {
-      amount: item.amount,
-      name,
-      price,
-      image,
-      quantity,
-      productID: _id,
-    };
-    // add item to order
-    orderItems = [...orderItems, singleOrderItem];
-    // calculate subtotal
-    total += item.amount * price;
-  }
+    const session = await mongoose.startSession();
+    await session.withTransaction(async () => {
+      // console.log(req.body);
+      const { items: cartItems } = req.body;
+      if (!cartItems || cartItems.length < 1) {
+        throw new CustomError.BadRequestError("No cart items provided");
+      }
+      let orderItems = [];
+      let total = 0;
+      for (const item of cartItems) {
+        const dbProduct = await Product.findOne({
+          _id: item.productId,
+        }).session(session);
+        if (!dbProduct) {
+          throw new CustomError.NotFoundError(
+            `No product with id : ${item.productId}`
+          );
+        }
 
-  const order = await Order.create({
-    orderItems,
-    total,
-    customerID: req.body.customerId,
-  });
-  res.status(StatusCodes.CREATED).json({ order });
+        // console.log(dbProduct);
+        const { name, price, image, _id, quantity } = dbProduct;
+        if (dbProduct.quantity < item.amount) {
+          return res.status(400).send("Not enough product in stock");
+        }
+
+        const singleOrderItem = {
+          name,
+          price,
+          quantity: item.amount,
+          productID: _id,
+        };
+        // add item to order
+        orderItems = [...orderItems, singleOrderItem];
+        //decrease quantity
+        dbProduct.quantity -= item.amount;
+        await dbProduct.save();
+        // calculate subtotal
+        total += item.amount * price;
+      }
+
+      const order = await Order.create({
+        orderItems,
+        total,
+        customerID: req.body.customerId,
+      });
+
+      res.status(StatusCodes.CREATED).json({ order });
+    });
+    session.endSession();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
 };
 module.exports = { getAllOrders, getSingleOrder, createOrder };
--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const SingleOrderItemSchema = mongoose.Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
+
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
   productID: {
