c5d394e1020bba23114c4380a07521ae19f46658
kebibragam <kebibragam@gmail.com>
2023-05-04 20:15:12 +0545

    castError

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,19 +1,9 @@
-// const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const CustomError = require("../errors");
 
 const { UnauthenticatedError } = require("../errors");
 
 const authenticateUser = async (req, res, next) => {
-  //check header
-  // const authHeader = req.headers.authorization;
-  // console.log(authHeader);
-
-  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
-  //   throw new UnauthenticatedError("Authentication invalid");
-  // }
-
-  // const token = authHeader.split(" ")[1];
   const token = req.cookies.token;
   if (!token) {
     throw new CustomError.UnauthenticatedError("Authentication Invalid");
@@ -35,10 +25,7 @@ const authenticateUser = async (req, res, next) => {
 };
 const authorizePermissions = (...roles) => {
   return (req, res, next) => {
-    // console.log(req.user);
-    // console.log(req.user.role);
     if (!roles.includes(req.user.role)) {
-      // console.log(req.user.role);
       throw new CustomError.UnauthenticatedError(
         "Unauthorized to access this route"
       );
--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,4 +1,3 @@
-// const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -7,9 +6,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong try again later",
   };
 
-  // if (err instanceof CustomAPIError) {
-  //   return res.status(err.statusCode).json({ msg: err.message });
-  // }
   if (err.name === "ValidationError") {
     // console.log(Object.values(err.errors));
     customError.msg = Object.values(err.errors)
@@ -24,11 +20,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400;
   }
   if (err.name === "CastError") {
-    customError.msg = `No item found with id ${err.value}`;
+    // console.log(err.value._id);
+    customError.msg = `No item found with id ${err.value._id}`;
     customError.statusCode = 404;
   }
-
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
