1578b95d5d57fb3de0f676f52e64ea25aa136845
kebibragam <kebibragam@gmail.com>
2023-06-13 07:44:17 +0545

    customer number added

--- models/Customer.js
+++ models/Customer.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const CustomerSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -14,6 +13,30 @@ const CustomerSchema = new mongoose.Schema({
     type: Number,
     required: [true, "Please provide product contact number"],
   },
+  number: {
+    type: Number,
+    unique: true, // Ensure uniqueness of auto-incrementing values
+  },
+});
+
+CustomerSchema.pre("save", async function (next) {
+  try {
+    if (this.isNew) {
+      const latestCustomer = await this.constructor.findOne(
+        {},
+        {},
+        { sort: { number: -1 } }
+      );
+      if (latestCustomer && latestCustomer.number) {
+        this.number = latestCustomer.number + 1;
+      } else {
+        this.number = 100;
+      }
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model("Customer", CustomerSchema);
